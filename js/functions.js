296cd541ed2c128b1ed220f7d1dd61c11c143fc8
mezoo adel <mo3tz3adel@gmail.com>
2022-12-26 08:32:46 +0200

    finished testing

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,12 @@ window.addEventListener('load', function () {
         // $(this).addClass('dropup text-primary')
     });
 
+    console.log("i'm Opening");
+
+   
+
 });//end Loading
+
 
 let navItem1 = this.document.querySelector("#collapseOne");
 let navItem2 = this.document.querySelector("#collapse2");
@@ -11,28 +16,33 @@ let navItem3 = this.document.querySelector("#collapse3");
 let navItem4 = this.document.querySelector("#collapse4");
 
 //this is a listener for @media query screen
-let listner = window.matchMedia("(max-width: 768px)");
+let listner = window.matchMedia("(max-width:768px)");
 
 //function will be used as callBack function
-function showLsItemsOfAccordion(listnr) {
-    if (listnr.matches) {
+function showLsItemsOfAccordion(e) {
+    console.log("i'm changing screen");
+    if (e.matches) {
         navItem1.classList.remove("show");
         navItem2.classList.remove("show");
         navItem3.classList.remove("show");
         navItem4.classList.remove("show");
+        console.log(e.matches);
 
     } else {
         navItem1.classList.add("show");
         navItem2.classList.add("show");
         navItem3.classList.add("show");
         navItem4.classList.add("show");
+    console.log(e);
+
     }
 }
 //add this is listener on @media query screen
-showLsItemsOfAccordion(listner);
-listner.addListener(showLsItemsOfAccordion);
+listner.addEventListener("change",showLsItemsOfAccordion);
 
-navItem1 = document.getElementsByClassName("toBeShown");
+//show more function
+function showMoreCategories(){
+    navItem1 = document.getElementsByClassName("toBeShown");
 navItem2 = navItem1[0];
 navItem3 = navItem1[1];
 navItem4 = navItem1[2];
@@ -56,3 +66,4 @@ navItem1.addEventListener("click", function () {
     navItem4.classList.toggle("d-none");
    
 });//onClick button closer
+}//close function
